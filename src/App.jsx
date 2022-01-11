302e38605fc4bd954f0e6b2ceeb131db5fd8c151
Doron Feldman <doronfeldman07@gmail.com>
2022-01-11 14:54:21 +0200

    question

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+
+import todos from './todos';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header text="TODO LIST" />
+      {/* Put you todo list here :) */}
+    </div>
+  );
+};
+
+export default App;
